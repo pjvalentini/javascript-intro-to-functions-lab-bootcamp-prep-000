e7da61bf4b891981eea1d42f56d9861e4785cf65
PJ Valentini <pj.valentini@gmail.com>
2018-06-11 21:07:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function sayHiToGrandma(string) {
       (string === "I love you Grandma.");
       
   }
-  return "I love you too";
+  return "I love you, too";
 }
 
 // function sayHiToGrandma(string) {
