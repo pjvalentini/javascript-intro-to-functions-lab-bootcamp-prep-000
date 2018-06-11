278549d0334b2b83022d0d93b139b7cdcdc36b83
PJ Valentini <pj.valentini@gmail.com>
2018-06-11 20:50:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string === string.ToLowerCase()) {
+  if (string === string.toLowerCase()) {
     return "I can\'t hear you!";
   }
 }
